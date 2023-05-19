85b5d24cd5ed1d47c97b2e8e84cf0bfa446d18b3
Vinit Patidar <117593724+Vinit-patidar-03@users.noreply.github.com>
2023-05-19 22:26:34 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -46,17 +46,17 @@ let getWeather = (city) => {
 			let body = document.getElementsByTagName('body');
 			let bg = document.getElementById('bg');
 			if (response.hour >= '06' && response.hour <= '18') {
-				body[0].style.background = "url('../images/Day.jpg') no-repeat center/cover"
+				body[0].style.background = "url('./images/Day.jpg') no-repeat center/cover"
 				bg.src = "../images/Day.png"
 			}
 			else if(response.hour >= '18') {
-				body[0].style.background = "url('../images/Night.jpg') no-repeat center/cover"
+				body[0].style.background = "url('./images/Night.jpg') no-repeat center/cover"
 				bg.src = "../images/Moon.png"
 
 			}
 			else
 			{
-				body[0].style.background = "url('../images/Day.jpg') no-repeat center/cover"
+				body[0].style.background = "url('./images/Day.jpg') no-repeat center/cover"
 				bg.src = "../images/Day.png"
 			}
 		})
